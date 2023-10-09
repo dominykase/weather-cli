5754d37b8f4b12994e3e26ff458c684c2a4c8322
Dominykas <dominykas.eicinas@gmail.com>
2023-10-09 21:00:35 +0300

    added a switch for the commands

--- src/main.go
+++ src/main.go
@@ -6,6 +6,30 @@ import (
 )
 
 func main() {
-    fmt.Println(messages.WelcomeMsg);
-    fmt.Println(messages.CommandsMsg);
+    var input string;
+
+    for input != "exit" {
+        switch (input) {
+            case "":
+                fmt.Println(messages.WelcomeMsg);
+                break;
+            case "search": 
+                break;
+            case "daily":
+                break;
+            case "hourly":
+                break;
+            default:
+                fmt.Println(
+                    fmt.Sprintf(
+                        "\n\"%s\" is not a recognised input.\n\n",
+                        input,
+                    ),
+                );
+                break;
+        }
+
+        fmt.Println(messages.CommandsMsg);
+        fmt.Scanln(&input);
+    }
 }
--- src/messages/messages.go
+++ src/messages/messages.go
@@ -8,5 +8,5 @@ var (
         fmt.Sprintf("\t%-32s%s", "search", "Search for cities.\n") +
         fmt.Sprintf("\t%-32s%s", "daily <location>", "Daily forecast for the next week. Provide city name in place of <location>.\n") +
         fmt.Sprintf("\t%-32s%s", "hourly <location>", "Hourly forecast for the next 24 hours. Provide city name in place of <location>.\n") +
-        fmt.Sprintf("\t%-32s%s", "exit", "Exit program.\n") 
+        fmt.Sprintf("\t%-32s%s", "exit", "Exit program.\n")
 )
